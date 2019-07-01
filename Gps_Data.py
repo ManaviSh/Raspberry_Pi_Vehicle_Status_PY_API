88817bc355816585d1ac9f0623301edb283362b3
Manavi Shukla <manavi139@gmail.com>
2019-07-02 04:58:00 +0530

    Update Gps_Data.py

--- Gps_Data.py
+++ Gps_Data.py
@@ -38,3 +38,12 @@ GPGGA_buffer = 0
 NMEA_buff = 0
 lat_in_degrees = 0
 long_in_degrees = 0
+
+def get_data():
+    received_data = (str)(ser.readline())                   #read NMEA string received
+    GPGGA_data_available = received_data.find(gpgga_info)   #check for NMEA GPGGA string                 
+    if (GPGGA_data_available>0):
+        GPGGA_buffer = received_data.split("$GPGGA,",1)[1]  #store data coming after "$GPGGA," string 
+        NMEA_buff = (GPGGA_buffer.split(','))               #store comma separated data in buffer
+        GPS_Info()                                          #get time, latitude, longitude
+ 
