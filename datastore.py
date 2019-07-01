6b6f8cd1cefa58fbc66c8a9d4825cc2ae78ee7d4
Manavi Shukla <manavi139@gmail.com>
2019-07-01 11:54:24 +0530

    Update datastore.py

--- datastore.py
+++ datastore.py
@@ -1,46 +1,145 @@
 
 import time
-
+import serial               #import serial pacakge
+import webbrowser           #import package for opening link in browser
+import sys
+import smbus
 import mysql.connector as mariadb
-
 mariadb_connection = mariadb.connect(user='USERNAME', password='PASSWORD', database='weather')
-
 cursor = mariadb_connection.cursor()
 
 
-readings = [
+#some MPU6050 Registers and their Address
+PWR_MGMT_1   = 0x6B
+SMPLRT_DIV   = 0x19
+CONFIG       = 0x1A
+GYRO_CONFIG  = 0x1B
+INT_ENABLE   = 0x38
+ACCEL_XOUT_H = 0x3B
+ACCEL_YOUT_H = 0x3D
+ACCEL_ZOUT_H = 0x3F
+GYRO_XOUT_H  = 0x43
+GYRO_YOUT_H  = 0x45
+GYRO_ZOUT_H  = 0x47
+
+def MPU_Init():
+	#write to sample rate register
+	bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
+	
+	#Write to power management register
+	bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
+	
+	#Write to Configuration register
+	bus.write_byte_data(Device_Address, CONFIG, 0)
+	
+	#Write to Gyro configuration register
+	bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
+	
+	#Write to interrupt enable register
+	bus.write_byte_data(Device_Address, INT_ENABLE, 1)
+
+def read_raw_data(addr):
+	#Accelero and Gyro value are 16-bit
+        high = bus.read_byte_data(Device_Address, addr)
+        low = bus.read_byte_data(Device_Address, addr+1)
+    
+        #concatenate higher and lower value
+        value = ((high << 8) | low)
+        
+        #to get signed value from mpu6050
+        if(value > 32768):
+                value = value - 65536
+        return value
+
+bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
+Device_Address = 0x68   # MPU6050 device address
+
+def GPS_Info():
+    global NMEA_buff
+    global lat_in_degrees
+    global long_in_degrees
+    nmea_time = []
+    nmea_latitude = []
+    nmea_longitude = []
+    nmea_time = NMEA_buff[0]                    #extract time from GPGGA string
+    nmea_latitude = NMEA_buff[1]                #extract latitude from GPGGA string
+    nmea_longitude = NMEA_buff[3]               #extract longitude from GPGGA string
+    
+    print("NMEA Time: ", nmea_time,'\n')
+    print ("NMEA Latitude:", nmea_latitude,"NMEA Longitude:", nmea_longitude,'\n')
+    
+    lat = float(nmea_latitude)                  #convert string into float for calculation
+    longi = float(nmea_longitude)               #convertr string into float for calculation
+    
+    lat_in_degrees = convert_to_degrees(lat)    #get latitude in degree decimal format
+    long_in_degrees = convert_to_degrees(longi) #get longitude in degree decimal format
+    
+#convert raw NMEA string into degree decimal format   
+def convert_to_degrees(raw_value):
+    decimal_value = raw_value/100.00
+    degrees = int(decimal_value)
+    mm_mmmm = (decimal_value - int(decimal_value))/0.6
+    position = degrees + mm_mmmm
+    position = "%.4f" %(position)
+    return position
+    
+gpgga_info = "$GPGGA,"
+ser = serial.Serial ("/dev/ttyS0")              #Open port with baud rate
+GPGGA_buffer = 0
+NMEA_buff = 0
+lat_in_degrees = 0
+long_in_degrees = 0
+MPU_Init()
+
+print (" Reading Data of Gyroscope and Accelerometer")
+
+while True:
+    #Read Accelerometer raw value
+    acc_x = read_raw_data(ACCEL_XOUT_H)
+    acc_y = read_raw_data(ACCEL_YOUT_H)
+    acc_z = read_raw_data(ACCEL_ZOUT_H)
+
+    #Read Gyroscope raw value
+    gyro_x = read_raw_data(GYRO_XOUT_H)
+    gyro_y = read_raw_data(GYRO_YOUT_H)
+    gyro_z = read_raw_data(GYRO_ZOUT_H)
+
+    #Full scale range +/- 250 degree/C as per sensitivity scale factor
+    Ax = acc_x/16384.0
+    Ay = acc_y/16384.0
+    Az = acc_z/16384.0
+
+    Gx = gyro_x/131.0
+    Gy = gyro_y/131.0
+    Gz = gyro_z/131.0
+
+
+    print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az) 	
+    sleep(1)
+    received_data = (str)(ser.readline())                   #read NMEA string received
+    GPGGA_data_available = received_data.find(gpgga_info)   #check for NMEA GPGGA string                 
+    if (GPGGA_data_available>0):
+        GPGGA_buffer = received_data.split("$GPGGA,",1)[1]  #store data coming after "$GPGGA," string 
+        NMEA_buff = (GPGGA_buffer.split(','))               #store comma separated data in buffer
+        GPS_Info()                                          #get time, latitude, longitude
+
+        print("lat in degrees:", lat_in_degrees," long in degree: ", long_in_degrees, '\n')             #press ctrl+c to plot on map and exit 
+        print("------------------------------------------------------------\n")
+    readings = [
     {
-        'id': 1,
-        'TempSensor1' : 1,
-        'TempSensor2' : 2,
-        'TempSensor3' : 3,
-        'TempSensorAvg' : 1.5,
-        'Humidity' : 22,
-        'Pressure' : 33,
-        'Altitude' : 44,
-        'SeaLevelPressure' : 55,
-        'Lux' : 66,
+        'angle_x' : Gx, 
+        'angle_y' : Gy,
+        'angle_z' : Gz,
+        'Latitude' : lat_in_degrees,
+        'Longitude' : long_in_degrees
         'TimeStamp' : time.strftime("%c")
     }
-]
-
-def getReadings(amount):
-    # This is where we will pull from:
-    #
-    # - Get readings (amount)
-    # - Get last hour
-    # - Get last day
-    # - Get last 7 days
-    #
-    #readings = cursor.execute("SELECT TempSensor1,TempSensor2,TempSensor3,TempSensorAvg,Humidity,Pressure,Altitude,SeaLevelPressure,Lux,TimeStamp FROM weather LIMIT amount")
-    #mariadb_connection.commit()
+ ]
+    def getReadings(amount):
     return readings
 
-def addReading(reading):
-
-    print reading.TempSensor1
-
-    cursor.execute("INSERT INTO reading (TempSensor1,TempSensor2,TempSensor3,TempSensorAvg,Humidity,Pressure,Altitude,SeaLevelPressure,Lux,TimeStamp) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,NOW())", (reading.TempSensor1,reading.TempSensor2,reading.TempSensor3,reading.TempSensorAvg,reading.Humidity,reading.Pressure,reading.Altitude,reading.SeaLevelPressure,reading.Lux))
+    def addReading(reading):
+    cursor.execute("INSERT INTO reading (angle_x,angle_y,angle_z,Latitude,Longitude,TimeStamp) VALUES (%s,%s,%s,%s,%s,NOW())", (reading.angle_x,reading.angle_y,reading.angle_z,reading.Latitude,reading.Longitude))
     mariadb_connection.commit()
     return reading
 
