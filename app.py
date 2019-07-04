d5b9e69d1c6a39ed95adee1da4bc4b0bed61cec3
Manavi Shukla <manavi139@gmail.com>
2019-07-04 23:03:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python
 from flask import Flask, jsonify, abort, make_response, request, url_for
 import time
@@ -7,119 +8,80 @@ import mysql.connector as mariadb
 import logging
 import simplejson as json
 logging.basicConfig(filename='api.log',level=logging.DEBUG)
-
 from flask_httpauth import HTTPBasicAuth
-
 auth = HTTPBasicAuth()
 app = Flask(__name__)
 
-readings = [
-    {
-        'id': 1,
-        'angle_x' : Gx, 
-        'angle_y' : Gy,
-        'angle_z' : Gz,
-        'acc_x' : Gx, 
-        'acc_y' : Gy,
-        'acc_z' : Gz,
-	    'velocity': velocity1,
-        'Latitude' : lat_in_degrees,
-        'Longitude' : long_in_degrees
-        'TimeStamp' : time.strftime("%c")
- ]
-## AUTH STUFF ###
-
 @auth.get_password
 def get_password(username):
-    if username == 'username':
-        return 'password'
-    return None
-
-
-@auth.error_handler
+    if username == 'web':
+        return 'secretpassword'
+    return None @auth.error_handler
 def unauthorized():
     return make_response(jsonify({'error': 'Unauthorized access'}), 401)
 ## 404 handler
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
-
 ## Function to add URI to each requests
 def make_public_reading(reading):
     new_reading = {}
     for field in reading:
         if field == 'id':
-            new_reading['uri'] = url_for('get_reading', reading_id=reading['id'], _external=True)
+            new_reading['uri'] = url_for('get_reading',
+reading_id=reading['id'], _external=True)
         else:
             new_reading[field] = reading[field]
     return new_reading
 
-
-## Routes
-## /weather/api/v1/readings  GET ALL
+## Routes /weather/api/v1/readings GET ALL
 @app.route('/weather/api/v1/readings', methods=['GET'])
 @auth.login_required
 def get_tasks():
-    #   return jsonify({'readings': [make_public_reading(reading) for reading in readings]})
+    #   return jsonify({'readings': [make_public_reading(reading) for
+    #   reading in readings]})
     return jsonify({'Readings': datastore.getReadings(2)})
-
-
 @app.route('/weather/api/v1/readings/<int:reading_id>', methods=['GET'])
 def get_task(reading_id):
-    reading = [reading for reading in readings if reading['id'] == reading_id]
-
+    reading = [reading for reading in readings if reading['id'] ==reading_id]
     if len(reading) == 0:
         abort(404)
     return jsonify({'reading': reading[0]})
-
-
 @app.route('/weather/api/v1/readings', methods=['POST'])
 def create_reading():
     status = 0
     logging.info('Started create_reading()')
-
-
     req_json = request.get_json()
-
     try:
-
-        mariadb_connection = mariadb.connect(user='username', password='password', database='weather')
-
+       mariadb_connection = mariadb.connect(user='web', password='secretpasswo$
         cursor = mariadb_connection.cursor()
-
-        cursor.execute("INSERT INTO reading (angle_x,angle_y,angle_z,Latitude,Longitude,TimeStamp) VALUES (%s,%s,%s,%s,%s,NOW())", (req_json['angle_x'],req_json['angle_y'],req_json['angle_z'],req_json['Latitude'],req_json['Longitude']))
-
+        cursor.execute("INSERT INTO reading (angle_x,angle_y,angle_z,acc_x,acc_$
         mariadb_connection.commit()
-
     except mariadb.Error as error:
         logging.error("Error: {}".format(error))
         return jsonify({'status': 'failed'}), 400
-
     except IOError as e:
         logging.error("I/O error({0}): {1}".format(e.errno, e.strerror))
         return jsonify({'status': 'failed'}), 400
-
     except:
         logging.error("Unexpected error:", sys.exc_info()[0])
         return jsonify({'status': 'failed'}), 400
-
     # if all is good
     return jsonify({'status': 'succeeded'}), 201
-@app.route('/hello', methods=['POST'])
+@app.route('/hello', methods=['GET','POST'])
 def index():
-   mariadb_connection = mariadb.connect(user='username', password='password', database='weather')
-
-        cursor = mariadb_connection.cursor()
-
-        cursor.execute("SELECT * FROM weather.reading")
+   mariadb_connection = mariadb.connect(user='web', password='secretpassword', $
+   cursor = mariadb_connection.cursor()
+   cursor.execute("SELECT * FROM reading")
 
    row_headers=[x[0] for x in cursor.description] #this will extract row headers
    rv = cursor.fetchall()
    json_data=[]
    for result in rv:
         json_data.append(dict(zip(row_headers,result)))
-   return json.dumps(json_data)
-
-
+   return jsonify(json_data)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',debug=True)
+
+
+
